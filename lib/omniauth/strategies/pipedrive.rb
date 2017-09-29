0eadc3892359a1cd19c37a675df3ba7e6b8463c1
Pranav Singh <pranav@supportbee.com>
2017-09-29 20:17:45 +0530

    Declares client options

--- lib/omniauth/strategies/pipedrive.rb
+++ lib/omniauth/strategies/pipedrive.rb
@@ -3,7 +3,15 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Pipedrive < OmniAuth::Strategies::OAuth2
-      include OmniAuth::Strategy
+      option :client_options, {
+        site: 'https://oauth.pipedrive.com',
+        authorize_url: 'https://oauth.pipedrive.com/oauth/authorize',
+        token_url: 'https://oauth.pipedrive.com/oauth/token'
+      }
+
+      def request_phase
+        super
+      end
     end
   end
 end
