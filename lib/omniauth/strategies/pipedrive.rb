38632c292067eae1195c70371a25e962d08abbac
Pranav Singh <pranav@supportbee.com>
2017-10-05 17:32:07 +0530

    Updates raw info

--- lib/omniauth/strategies/pipedrive.rb
+++ lib/omniauth/strategies/pipedrive.rb
@@ -29,7 +29,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/users').parsed
+        @raw_info ||= access_token.params['data']
       end
     end
   end
