b4030101fcf7a875a8836bfe3756cca9099898f5
Nisanth Chunduru <nisanth074@gmail.com>
2019-03-19 21:00:26 +0530

    Ignore my personal rubygems credentials when publishing a new version of the gem

--- script/publish_to_ruby_gems.sh
+++ script/publish_to_ruby_gems.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Publish a new version of the gem
+# script/publish_to_ruby_gems.sh
+
+mv ~/.gem/credentials ~/.gem/credentials.orig
 gem build omniauth-pipedrive.gemspec
 gem push omniauth-pipedrive-$(cat VERSION).gem
+mv ~/.gem/credentials.orig ~/.gem/credentials
